aa34929a35eac0b927e4da5aeccfdfb002daddca
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-12-15 13:13:35 +0300

    Popa

--- medvedik_1laba_8sem/medvedik_1laba_8sem.cpp
+++ medvedik_1laba_8sem/medvedik_1laba_8sem.cpp
@@ -1,16 +1,18 @@
 ﻿#include <iostream>
 #include<cmath>
 //#include"IntegraL.h"
+using namespace std;
 
 const double pi = acos(-1);
 const int N = 10;
+
 double x[N]; 
 double a = -1;
 double b = 1;
 double h = (b - a) / N;
 
 double f(double y) {
-	return y + sqrt(1 - y * y);
+	return (y + sqrt(1 - y * y));
 }
 
 double Tn(int n, double x) {
@@ -21,13 +23,28 @@ double Un(int n, double x) {
     return (sin((n + 1) * acos(x)) / sqrt(1 - x * x));
 }
 
-double Mid(double h, double x1, double x2, int n) {
+double Mid(double h1, double x1, double x2, int n) { //perepisat' integral
 	double S = 0.0;
-	for (double i = x1; i < x2; i = i + h)
+	h1 = (b - a) / N;
+	for (double i = x1; i <= x2; i = i + h1)
 	{
-		S += (f(i + (h / 2.0))*Tn(n, i)/(sqrt(1 - i * i)));
+		if ((i * i) != 1.0) {
+			S += (f(i + (h1 / 2.0)) * Tn(n, i) / (sqrt(1 - i * i)));
+			//cout << "jopa1 " << f(i + (h1 / 2.0)) << endl;
+			//cout << "jopa2 " << Tn(n, i) << endl;
+			//cout << "jopa3 " << (sqrt(1 - i * i)) << endl;
+			//cout << "jopaS = " << S << endl;
+		}
+		else
+		{
+
+			S += 0;
+			//cout <<"\nJOPA = " << (f(i + (h / 2.0)) * Tn(n, i) / (sqrt(1.0 - ((i * i) + 0.0001)))) << endl;
+			//cout << 1.0 - ((i * i) - 0.0001) << endl;
+		}
+
 	}
-	return (S * h);
+	return (S * h1);
 }
 
 double Mid2(double h, double x1, double x2, int n) {
@@ -40,13 +57,14 @@ double Mid2(double h, double x1, double x2, int n) {
 }
 
 
-double an(int n, double x) {
-
+double an(int n) {
+	//cout << Mid(0.1, -1, 1, n) << endl;
 	return (2.0 / pi) * Mid(0.01, -1, 1, n);
+
 
 }
 
-double bn(int n, double x) {
+double bn(int n) {
 
 	return (2.0 / pi) * Mid2(0.01, -1, 1, n);
 
@@ -75,40 +93,46 @@ double KK() {
 
 int main()
 {
-	//double fi = 0.0;
-	//for (int i = 0; i < N; i++)
-	//{
-	//	x[i] = a + i * h;
-	//}
-	//fi = f(0) * Tn(0, x[0]) / 2 * log(2);
-	//for (int i = 1; i < N; i++)
-	//{
-	//	fi += f(y) * Tn(i, y);
-	//}
+	double fi[N]; // eto doljno bit ryadom i bit prakticheski sovpadat' s tipafii
+	for (int i = 0; i < N; i++)
+	{
+		x[i] = a + i * h;
+	}
+	fi[0] = f(0) * Tn(0, x[0]);
+	for (int i = 1; i < N; i++)
+	{
+		fi[i] += f(x[i]) * Tn(i, x[i]);
+	}
 	double Tipafii[N];
 	for (int i = 0; i < N; i++) {
 		if (i==0)
 		{
-			double o = an(0, x[0]) / 2.0;
+			double o = an(0) / 2.0;
+			//cout << o << endl;
+			//cout << Tn(1,x[1]) << endl;
 			Tipafii[i] = o * Tn(i, x[i]);
 		}
 		else
 		{
-			Tipafii[i] += an(i, x[i]) * Tn(i, x[i]);
+			//cout << "an = " << an(i) << " tn = " << Tn(i, x[i]) << endl;
+			Tipafii[i] += an(i) * Tn(i, x[i]);
 		}
 		//Tipafii[i] +=  * Tn(0, x[0]);
 	}
+	/*for (int i = 0; i < N; i++) {
+		cout << "t= " << Tn(i, 0.1) << endl;
+	
+	}*/
 
+	//Tipafii[0] = (an(0, x[0]) * Tn(0, x[0])) / 2.0;
+	for (int i = 0; i < N; i++) {
 
-
-	Tipafii[0] = (an(0, x[0]) * Tn(0, x[0])) / 2.0;
-	for (int i = 1; i < N; i++) {
-
-		fii[i] = f(x[i]) * Tn(i, x[i]);
-		printf("%.110f\n", fii[i]);
+		//fii[i] = f(x[i]) * Tn(i, x[i]);
+		cout << "take :"<< i << endl;
+		printf("%.110f\n", Tipafii[i]);
 		std::cout << "\n";
-		printf("%.110f\n", f(x[i]));
-		
+		printf("%.110f\n", fi[i]);
+		std::cout << "\n";
 	}
 	
 	return 1;
