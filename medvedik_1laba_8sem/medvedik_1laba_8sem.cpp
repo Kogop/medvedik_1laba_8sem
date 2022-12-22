4b71262168b3525787a2be901aef829f322c9caa
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-12-22 13:17:40 +0300

    5 кратно переваренный кал

--- medvedik_1laba_8sem/medvedik_1laba_8sem.cpp
+++ medvedik_1laba_8sem/medvedik_1laba_8sem.cpp
@@ -13,6 +13,7 @@ double h = (b - a) / N;
 
 double f(double y) {
 	return (y + sqrt(1 - y * y));
+	//return y * y;
 }
 
 double Tn(int n, double x) {
@@ -24,26 +25,26 @@ double Un(int n, double x) {
 }
 
 double middlepryam1(double a, double b, double n) {    //4el 4to esli v integrale x dolgen bit strogo zadan a menyatsya tolko n doljno?
-	double nn = 10, h, x, x1;
+	double nn = 1000, h, x, x1;
 	double S = 0.0;
 	h = (b - a) / nn;
 	x = a + (h / 2);
 	//cout<<" x= "<<x<<endl;
 	while (x < b - h / 2.0) {
-		if ((x * x) != 1.0 && (x + h / 2.0) != 1.0) {
-			S += (f(x + (h / 2.0)) * Tn(n, x)) / (sqrt(1 - x * x));
-			//cout << "jopa1 " << f(x + (h / 2.0)) << endl;
-			//cout << "jopa2 " << Tn(n, i) << endl;
-			//cout << "jopa3 " << (sqrt(1 - i * i)) << endl;
-			//cout << "jopaS = " << S << endl;
-		}
-		else
-		{
+		//if ((x * x) != 1.0 && (x + h / 2.0) != 1.0) {
+		S += (f(x + (h / 2.0)) * Tn(n, x)) / (sqrt(1 - x * x));
+		//cout << "jopa1 " << f(x + (h / 2.0)) << endl;
+		//cout << "jopa2 " << Tn(n, i) << endl;
+		//cout << "jopa3 " << (sqrt(1 - i * i)) << endl;
+		//cout << "jopaS = " << S << endl;
+	//}
+	//else
+	//{
 
-			S += 0.0;
-			//cout <<"\nJOPA = " << (f(i + (h / 2.0)) * Tn(n, i) / (sqrt(1.0 - ((i * i) + 0.0001)))) << endl;
-			//cout << 1.0 - ((i * i) - 0.0001) << endl;
-		}
+	//	S += 0.0;
+	//	//cout <<"\nJOPA = " << (f(i + (h / 2.0)) * Tn(n, i) / (sqrt(1.0 - ((i * i) + 0.0001)))) << endl;
+	//	//cout << 1.0 - ((i * i) - 0.0001) << endl;
+	//}
 		x += h;
 	}
 	return S * h;
@@ -99,7 +100,9 @@ double bn(int n) {
 }
 
 
-
+double ffn(int n) {
+	return (an(n) / n);
+}
 
 
 
@@ -121,45 +124,44 @@ double KK() {
 
 int main()
 {
-	double fi[N]; // eto doljno bit ryadom i bit prakticheski sovpadat' s tipafii
+	//double fi[N]; // eto doljno bit ryadom i bit prakticheski sovpadat' s tipafii
 	for (int i = 0; i < N; i++)
 	{
 		x[i] = a + i * h;
 	}
-	fi[0] = f(0) * Tn(0, x[0]);
-	for (int i = 1; i < N; i++)
-	{
-		fi[i] = f(x[i]) * Tn(i, x[i]);
-	}
-	double Tipafii[N];
+	//fi[0] = f(0) * Tn(0, x[0]);
+	//for (int i = 1; i < N; i++)
+	//{
+	//	fi[i] = f(x[i]) * Tn(i, x[i]);
+	//}
+	double Tipafii[N] = {0,0,0,0,0,0,0,0,0,0};
 	for (int i = 0; i < N; i++) {
-		if (i == 0)
-		{
-			double o = an(0) / 2.0;
-			//cout << o << endl;
-			//cout << Tn(1,x[1]) << endl;
-			Tipafii[i] = o * Tn(i, x[i]);
-		}
-		else
-		{
+
+		double o = an(0)/ 2.0;
+		//cout << o << endl;
+		//cout << Tn(1,x[1]) << endl;
+		Tipafii[i] = o * Tn(0, x[i]);
+
+		for (int j = 1; j < N; j++) {
 			//cout << "an = " << an(i) << " tn = " << Tn(i, x[i]) << endl;
-			Tipafii[i] = an(i) * Tn(i, x[i]);
+			Tipafii[i] += an(j) * Tn(j, x[i]);
 		}
-		//Tipafii[i] +=  * Tn(0, x[0]);
+
+	//Tipafii[i] +=  * Tn(0, x[0]);
 	}
 	/*for (int i = 0; i < N; i++) {
 		cout << "t= " << Tn(i, 0.1) << endl;
 	
 	}*/
-
+	cout << middlepryam1(-1, 1, 10) << endl;
 	//Tipafii[0] = (an(0, x[0]) * Tn(0, x[0])) / 2.0;
 	for (int i = 0; i < N; i++) {
 
 		//fii[i] = f(x[i]) * Tn(i, x[i]);
 		cout << "take :"<< i << endl;
-		printf("%.110f\n", Tipafii[i]);
+		printf("%.5f\n", Tipafii[i]);
 		std::cout << "\n";
-		printf("%.110f\n", fi[i]);
+		printf("%.5f\n", f(x[i]));
 		std::cout << "\n";
 	}
 	
